7f8f4f8fd798088992f4bacd4c87c7cf4e745709
Géraud Ilinca <geraud.ilinca@telecom-paris.fr>
2025-04-30 11:50:18 +0200

    remove finite time criterion

--- solvers/sklearn_ipca.py
+++ solvers/sklearn_ipca.py
@@ -1,17 +1,11 @@
 from benchopt import BaseSolver, safe_import_context
-from benchopt.stopping_criterion import StoppingCriterion
+from benchopt.stopping_criterion import NoCriterion
 
 with safe_import_context() as import_ctx:
     import numpy as np
     from sklearn.decomposition import IncrementalPCA
     from sklearn.utils import gen_batches
 
-class FiniteTimeStoppingCriterion(StoppingCriterion):
-    def check_convergence(self, objective_list):
-        if not hasattr(self, "n_iter") or self.n_iter is None : 
-            raise Exception("n_iter must be set in the solver")
-        current_iter = len(objective_list)
-        return False, current_iter / self.n_iter ## if current_iter == len(objective_list), can also rely directly on it
 
 
 class Solver(BaseSolver):
@@ -23,7 +17,7 @@ class Solver(BaseSolver):
 
     requirements = ["scikit-learn"]
 
-    stopping_criterion = FiniteTimeStoppingCriterion(strategy="callback")
+    stopping_criterion = NoCriterion(strategy="callback")
 
     def get_next(self, stop_val):
         return stop_val + 1
@@ -32,15 +26,13 @@ class Solver(BaseSolver):
         self.X, self.n_components = X, n_components
 
     def pre_run_hook(self, callback):
-        n_batch = int(np.ceil(self.X.shape[0] / self.batch_size)) 
-        callback.stopping_criterion.n_iter = n_batch
+        self.ipca_transform = IncrementalPCA(n_components=self.n_components, batch_size=self.batch_size)
 
     def run(self, callback):
-        ipca_transform = IncrementalPCA(n_components=self.n_components, batch_size=self.batch_size)
         for batch_slice in gen_batches(self.X.shape[0], self.batch_size):
-            ipca_transform.partial_fit(self.X[batch_slice])
-            self.pca = ipca_transform.components_
-            self.var_ratio = ipca_transform.explained_variance_ratio_
+            self.ipca_transform.partial_fit(self.X[batch_slice])
+            self.pca = self.ipca_transform.components_
+            self.var_ratio = self.ipca_transform.explained_variance_ratio_
             callback()
 
     def get_result(self):
--- solvers/sklearn_pca.py
+++ solvers/sklearn_pca.py
@@ -2,7 +2,6 @@ from benchopt import BaseSolver, safe_import_context
 
 
 with safe_import_context() as import_ctx:
-    import numpy as np
     from sklearn.decomposition import PCA
 
 # source : 
