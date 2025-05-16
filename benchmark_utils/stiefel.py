56e401654cdac7368522d5a3e3f9feb5991b62dd
Géraud Ilinca <geraud.ilinca@telecom-paris.fr>
2025-05-16 17:37:44 +0200

    better naming convention for var in stiefel utils

--- benchmark_utils/stiefel.py
+++ benchmark_utils/stiefel.py
@@ -4,64 +4,64 @@ import scipy.linalg
 # Source of Retractions : https://link.springer.com/article/10.1007/s11590-018-1341-z
 
 
-def tangent_proj(W: np.ndarray, X: np.ndarray) -> np.ndarray :
+def tangent_proj(G: np.ndarray, W: np.ndarray) -> np.ndarray :
     """Projection on tangent space of Stiefel Manifold
 
     Args:
-        W (np.ndarray): Matrix (d, k) to project
-        X (np.ndarray): Point (d, k) of the Stiefel Manifold for which we consider the tangent space
+        G (np.ndarray): Matrix (d, k) to project
+        W (np.ndarray): Point (d, k) of the Stiefel Manifold for which we consider the tangent space
 
     Returns:
-        np.ndarray: Projection  (d, k) of W on tangent space at X
+        np.ndarray: Projection  (d, k) of G on tangent space at W
     """
-    xw = X.T@W 
-    proj = W - X@((xw + xw.T)/2) 
+    wg = W.T@G
+    proj = G - W@((wg + wg.T)/2) 
     return proj 
 
 
-def _qr_retraction(X: np.ndarray, V: np.ndarray) -> np.ndarray: 
-    Q, _ = np.linalg.qr((X+V), mode="reduced")
+def _qr_retraction(W: np.ndarray, G: np.ndarray) -> np.ndarray: 
+    Q, _ = np.linalg.qr((W+G), mode="reduced")
     return Q
 
-def _polar_retraction(X: np.ndarray, V: np.ndarray) -> np.ndarray:
-    eig, U = np.linalg.eigh(np.eye(X.shape[1]) + V.T @ V)
+def _polar_retraction(W: np.ndarray, G: np.ndarray) -> np.ndarray:
+    eig, U = np.linalg.eigh(np.eye(W.shape[1]) + G.T @ G)
     squared_root_inv = U @ np.diag(1/np.sqrt(eig)) @ U.T
-    return (X + V) @ squared_root_inv
+    return (W + G) @ squared_root_inv
 
-def _A(X: np.ndarray, V:np.ndarray) -> np.ndarray:
-    pvx = (np.eye(X.shape[0]) - (X @ X.T)/2) @ V @ X.T 
-    A = pvx - pvx.T 
+def _A(W: np.ndarray, G:np.ndarray) -> np.ndarray:
+    pgw = (np.eye(W.shape[0]) - (W @ W.T)/2) @ G @ W.T 
+    A = pgw - pgw.T 
     return A
 
-def _cayley_retraction(X: np.ndarray, V: np.ndarray) -> np.ndarray: 
+def _cayley_retraction(W: np.ndarray, G: np.ndarray) -> np.ndarray: 
     #TODO: Implement efficient implementation based on the fact that rank A <= 2k (SMW Formula)
     # source : https://repository.rice.edu/server/api/core/bitstreams/3d52e0a2-b1c6-45ce-84f7-42774b933099/content
-    A = _A(X, V)
-    R = np.linalg.inv(np.eye(X.shape[0]) - A/2) @ (np.eye(X.shape[0]) + A/2) @ X
+    A = _A(W, G)
+    R = np.linalg.inv(np.eye(W.shape[0]) - A/2) @ (np.eye(W.shape[0]) + A/2) @ W
     return R
 
-def _exp_retraction(X: np.ndarray, V: np.ndarray) -> np.ndarray: 
-    A = _A(X, V)
-    R = scipy.linalg.expm(A) @ X
+def _exp_retraction(W: np.ndarray, G: np.ndarray) -> np.ndarray: 
+    A = _A(W, G)
+    R = scipy.linalg.expm(A) @ W
     return R
 
-def retraction(X: np.ndarray, V: np.ndarray, mode="QR") -> np.ndarray:
+def retraction(W: np.ndarray, G: np.ndarray, mode="QR") -> np.ndarray:
     """Approximate exponential map on the Stiefel manifold
 
     Args:
-        X (np.ndarray): Departure point on the Stiefel manifold
-        V (np.ndarray): Vector in the tangent space of X 
+        W (np.ndarray): Departure point on the Stiefel manifold
+        G (np.ndarray): Vector in the tangent space of W
         mode (str, optional): Type of retraction, one of ["QR", "polar", "cayley", "exp"]. Defaults to "QR".
 
     Returns:
-        np.ndarray: Point of the Stiefel manifold, obtained by following V direction from X point
+        np.ndarray: Point of the Stiefel manifold, obtained by following G direction from W point
     """
     if mode == "QR":
-        return _qr_retraction(X, V)
+        return _qr_retraction(W, G)
     if mode == "polar":
-        return _polar_retraction(X, V)
+        return _polar_retraction(W, G)
     if mode == "cayley" :
-        return _cayley_retraction(X, V)
+        return _cayley_retraction(W, G)
     if mode == "exp" : 
-        return _exp_retraction(X, V)
+        return _exp_retraction(W, G)
     raise ValueError("'mode' should be one of ['QR', 'polar', 'cayley', 'exp']")
