3ba501e700581597af1397df340d36030aa3e77f
Géraud Ilinca <geraud.ilinca@telecom-paris.fr>
2025-05-21 11:57:12 +0200

    vrpca

---
+++ benchmark_utils/utils.py
@@ -0,0 +1,5 @@
+import numpy as np 
+
+def inv_squared_root(M: np.ndarray):
+    U, S, Vh = np.linalg.svd(M)
+    return Vh.T @ np.diag(1/np.sqrt(S)) @ U.T 
---
+++ solvers/vrpca.py
@@ -0,0 +1,54 @@
+from benchopt import BaseSolver, safe_import_context
+
+with safe_import_context() as import_ctx:
+    import numpy as np 
+    import scipy.stats 
+    from benchmark_utils import constants, utils
+
+# Pseudo-code from https://proceedings.mlr.press/v48/shamira16.pdf
+class Solver(BaseSolver):
+
+    name = 'VR-PCA'
+
+    parameters = {
+        "step_size" : [1e-4, 1e-3, 1e-2],
+        "epoch_size" : [10, 100, 1000], # still way less than indicated in paper ~ n
+        "random_seed" : [constants.RANDOM_SEED]
+        }
+
+    requirements = ["scipy"]
+
+    sampling_strategy = "iteration"
+
+    def set_objective(self, X, n_components):
+        self.X = X
+        self.n_components = n_components
+            
+
+    def run(self, n_iter):
+        generator = np.random.default_rng(self.random_seed)
+        n, d = self.X.shape
+        k = self.n_components
+
+        X = self.X.T
+
+        ortho_generator = scipy.stats.ortho_group(max(k, d), generator)
+        W_tilde = ortho_generator.rvs()[:d, :k]
+
+        indices = generator.integers(0, n, (n_iter, self.epoch_size)) #TODO: Add online version
+
+        for i in range(n_iter):
+            U_tilde = X@(X.T@W_tilde)/n 
+            W = W_tilde 
+            for j in range(self.epoch_size):
+                U, _, Vh = np.linalg.svd(W.T@W_tilde, full_matrices=False, compute_uv=True)
+                B = Vh.T@U.T 
+                x = self.X[indices[i,j]]
+                W_prime = W + self.step_size*(x[:, None] @ (x[None, :] @ W - x[None, :] @ (W_tilde @ B)) + U_tilde @ B)
+                W = W_prime @ utils.inv_squared_root(W_prime.T@W_prime)
+            W_tilde = W 
+
+        self.components = W_tilde
+
+    def get_result(self):
+        return dict(components=self.components)
