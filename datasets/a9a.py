455a2b9a52809f06a41a0eee4e72cc8463b340da
Géraud Ilinca <geraud.ilinca@telecom-paris.fr>
2025-05-23 18:03:06 +0200

    clean a9a

--- datasets/a9a.py
+++ datasets/a9a.py
@@ -4,11 +4,6 @@ from benchopt.config import get_data_path
 with safe_import_context() as import_ctx:
     from sklearn.datasets import fetch_openml
 
-def get_a9a(data_home):
-    a9a = fetch_openml(name="a9a", data_home=data_home)
-    a9a = a9a.data.toarray()
-    return a9a
-
 class Dataset(BaseDataset):
 
     name = "a9a"
